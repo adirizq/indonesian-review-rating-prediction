e6b5bb7eb777df88b3e1946336e094bdfe3a8355
Rizky Adi <rizkyzadadi@gmail.com>
2022-11-07 10:06:28 +0700

    Fixing lstm preprocessor and adding bert preprocessor

--- utils/preprocess_bert.py
+++ utils/preprocess_bert.py
@@ -5,7 +5,7 @@ import pytorch_lightning as pl
 import pandas as pd
 import numpy as np
 
-from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from torch.utils.data import TensorDataset, DataLoader
 from transformers import BertTokenizer
 from gensim.models import word2vec
 from keras.utils import pad_sequences
@@ -28,27 +28,31 @@ class ReviewDataModule(pl.LightningDataModule):
         data = dataset[['review_content', 'rating']]
         data = data.dropna()
 
-        y = []
+        x_input_ids, x_token_type_ids, x_attention_mask, y = [], [], [], []
 
-        print('Fixing Label...')
-        for rating in tqdm(data['rating'], desc='Fixing Label'):
-            y.append(rating-1)
-        print('[Fixing Completed]\n')
+        for i, tr_d in enumerate(tqdm(data.values.tolist())):
+            review = tr_d[0]
+            label = tr_d[1]
 
-        tokenizer = Tokenizer()
-        fit_text = data['review_content']
-        tokenizer.fit_on_texts(fit_text)
-        sequences = tokenizer.texts_to_sequences(data['review_content'])
-        list_set_sequence = [list(dict.fromkeys(seq)) for seq in sequences]
+            encoded_lbl = [0] * 5
+            encoded_lbl[label-1] = 1
 
-        print('Padding Data...')
-        x = pad_sequences([list(list_set_sequence[i]) for i in range(len(list_set_sequence))], maxlen=self.max_len, padding='pre')
-        print('[Padding Completed]\n')
+            tkn = self.tokenizer(text=review,
+                                 max_length=self.max_length,
+                                 padding='max_length',
+                                 truncation=True)
 
-        x = torch.tensor(x)
+            x_input_ids.append(tkn['input_ids'])
+            x_token_type_ids.append(tkn['token_type_ids'])
+            x_attention_mask.append(tkn['attention_mask'])
+            y.append(encoded_lbl)
+
+        x_input_ids = torch.tensor(x_input_ids)
+        x_token_type_ids = torch.tensor(x_token_type_ids)
+        x_attention_mask = torch.tensor(x_attention_mask)
         y = torch.tensor(y)
 
-        tensor_dataset = TensorDataset(x, y)
+        tensor_dataset = TensorDataset(x_input_ids, x_token_type_ids, x_attention_mask, y)
 
         print('Splitting Data...')
         train_valid_len = round(len(tensor_dataset) * 0.8)
@@ -71,37 +75,6 @@ class ReviewDataModule(pl.LightningDataModule):
         print('[Splitting Completed]\n')
 
         return train_data, valid_data, test_data
-
-    def word_embedding(self):
-        dataset = pd.read_csv(f"{self.dataset_dir}/preprocessed_reviews.csv")
-        data = dataset[['review_content', 'rating']]
-        data = data.dropna()
-
-        tokenizer = Tokenizer()
-        fit_text = data['review_content']
-        tokenizer.fit_on_texts(fit_text)
-
-        print('Loading Word2Vec Model...')
-        w2v_model = word2vec.Word2Vec.load('models/w2v/idwiki_word2vec_200_new_lower.model')
-        w2v_weights = w2v_model.wv
-        w2v_vocab_size, embedding_size = w2v_weights.vectors.shape
-        print('[Loading Completed]\n')
-
-        jumlah_index = len(tokenizer.word_index) + 1
-
-        embedding_matrix = np.zeros((jumlah_index, 200))
-        print('Creating Embedding Matrix...')
-        for word, i in tqdm(tokenizer.word_index.items(), desc='Creating W2V Weigth'):
-            try:
-                embedding_vector = w2v_weights[word]
-                embedding_matrix[i] = embedding_vector
-            except KeyError:
-                embedding_matrix[i] = np.random.normal(0, np.sqrt(0.25), 200)
-        print('[Embedding Matrix Completed]\n')
-
-        del (w2v_weights)
-
-        return embedding_matrix, w2v_vocab_size, embedding_size
 
     def setup(self, stage=None):
         train_data, valid_data, test_data = self.load_data()
